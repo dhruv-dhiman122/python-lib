a56d00d587c9784d82a401fa918fe99f0100722c
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-07-07 18:40:03 +0530

    the code for differenation is written

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import sympy as sp
 def sqrt_func(x):
     print(f"the square root of number {x} is {sp.sqrt(x)}")
 
-user_num = int(input("Enter the number you want a square root of "))
-sqrt_func(user_num)
+x,y,z = sp.symbols('x y z')
+sp.init_printing(use_unicode=True)
+print(sp.diff(sp.sin(x)*sp.exp(x),x))
