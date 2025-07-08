eb94e1d8a83ae89d90938e400041a523da6534a8
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-07-08 07:17:36 +0530

    the code for subs is written

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import sympy as sp
 
-def sqrt_func(x):
-    print(f"the square root of number {x} is {sp.sqrt(x)}")
+sp.init_printing(use_unicode=True)
 
 x,y,z = sp.symbols('x y z')
-sp.init_printing(use_unicode=True)
-print(sp.limit(sp.sin(x)/x,x,0))
-print(sp.limit(sp.sin(x)/sp.tan(x),x,0))
+mat = x + 1
+print(mat.subs(x,2))
