7d6cebe427f77dcb4c0bc8f7e38753c9da6340b5
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-07-08 07:54:21 +0530

    the code for substitution of sin x is wirtten

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sympy as sp
 sp.init_printing(use_unicode=True)
 
 x,y,z = sp.symbols('x y z')
-exp = (x*2)**2+x*y*(4*y)
+exp = sp.sin(2*x) + sp.cos(2*x)
 exp2 = x**y
 print(exp2.subs(y,x**(x**x)))
+print(exp.subs(sp.sin(2*x),2*sp.sin(x)*sp.cos(x)))
