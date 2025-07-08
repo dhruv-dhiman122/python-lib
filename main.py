cac947642daaff718336b38e1a61f26e7cf59022
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-07-08 07:50:45 +0530

    the code for substitution is written

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import sympy as sp
 sp.init_printing(use_unicode=True)
 
 x,y,z = sp.symbols('x y z')
-mat = x + 1
-print(mat.subs(x,2))
+exp = (x*2)**2+x*y*(4*y)
+exp2 = x**y
+print(exp2.subs(y,x**(x**x)))
