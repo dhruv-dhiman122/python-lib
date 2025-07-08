e2d197f27850810fb367156b157bc3a3059fc4a6
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-07-08 08:02:31 +0530

    the code for evalf is written

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import sympy as sp
 sp.init_printing(use_unicode=True)
 
 x,y,z = sp.symbols('x y z')
+#using subs
 exp = sp.sin(2*x) + sp.cos(2*x)
 exp2 = x**y
 print(exp2.subs(y,x**(x**x)))
 print(exp.subs(sp.sin(2*x),2*sp.sin(x)*sp.cos(x)))
+#using evalf
+exp = sp.sqrt(8)
+print(exp.evalf())
