99f1c6cd311c04eac06f6946fb1f763f057789e8
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-07-22 08:45:29 +0530

    the core understanding of array was tested

--- num.py
+++ num.py
@@ -1,1 +1,7 @@
 import numpy as np
+#print(np.__version__)
+l1 = [1,2,3,4]
+l2 = ['dhruv',10] # this will not give error 
+l3 = [[1,2,3],[4,5]] # this will give the error because the 2 D matrix should have the same number of rows and columns
+np_arr = np.array(l2)
+print(np_arr)
